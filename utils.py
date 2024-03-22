0598be066da0152a3c92bcea947f181d9ea29b41
Silvio Caprara <silvio.caprara@edu.unito.it>
2024-03-22 10:27:31 +0100

    Request set images code
    TODO: Download images, then build model

--- utils.py
+++ utils.py
@@ -6,6 +6,7 @@ load_dotenv()
 headers = {'X-API-Key': os.getenv("POKEMON_TCG_API_KEY")}
 print(headers)
 
+
 def build_models():
     response = requests.get("https://api.pokemontcg.io/v2/sets?q=legalities.standard:legal", headers=headers)
     if response.status_code == 200:
@@ -15,9 +16,19 @@ def build_models():
     else:
         print("Failed to retrieve data. Status code:", response.status_code)
 
-def build_model(set_id):
-    if(set_id == "sv4pt5"): #Testing
-        print(set_id)
 
-def download_set_cards_images(set):
-    print("c")
+def build_model(set_id):
+    if (set_id == "sv4pt5"):  # Testing
+        download_set_cards_images(set_id)
+
+
+def download_set_cards_images(set_id):
+    response = requests.get("https://api.pokemontcg.io/v2/cards?q=set.id:sv4pt5", headers=headers)
+    if response.status_code == 200:
+        if not os.path.exists(os.path.join("card_sets", set_id)):
+            os.makedirs(os.path.join("card_sets", set_id))
+        data = response.json().get('data', [])
+        for card in data:
+            print(card.get('images').get('large'))
+    else:
+        print("Failed to retrieve data. Status code:", response.status_code)
