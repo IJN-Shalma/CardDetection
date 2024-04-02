51453fbe3380f5ba6cba8ac6a5d1d357ab59fee1
Silvio Caprara <silvio.caprara@edu.unito.it>
2024-04-02 21:39:55 +0200

    Card perspective transform and extraction

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import utils
 import cv2
 
-# utils.build_sets_hashes()
-# utils.compile_card_set_hashes("sv4pt5")
+#utils.build_sets_hashes()
+#utils.compile_card_set_hashes("sv4pt5")
 #print(help(utils.build_sets_hashes))
 image = cv2.imread("test/2.jpg")
 utils.find_cards_in_image(image)
--- utils.py
+++ utils.py
@@ -1,8 +1,10 @@
+import math
 import os
 
 from PIL import Image
 from tqdm import tqdm
 from dotenv import load_dotenv
+import numpy as np
 import cv2
 import shutil
 import requests
@@ -53,9 +55,10 @@ def compile_card_set_hashes(set_id):
     """
     images_data = []
     for card_image in os.listdir(os.path.join("card_sets", set_id)):
-        image_hash = imagehash.phash(Image.open(os.path.join("card_sets", set_id, card_image)))
+        image = Image.open(os.path.join("card_sets", set_id, card_image))
+        image_hash = imagehash.phash(image)
         images_data.append({
-            'card_id': card_image,
+            'card_id': card_image.rsplit('.', 1)[0],
             'hash': str(image_hash)
         })
     with open(os.path.join("card_sets", set_id, set_id + ".json"), 'w') as json_file:
@@ -102,16 +105,40 @@ def find_cards_in_image(image):
     From a camera image, returns all the cards identified in the image
 
     Args:
-        image (NumPy array): Camera image
-
-    TODO: Complete image processing to scan and identify card.
+        image: Camera image
     """
     image = cv2.resize(image, (1270, 720), interpolation=cv2.INTER_AREA)
     contours = find_contours(image)
     images = extract_card_images_from_image(contours, image)
     for image in images:
         cv2.imshow("Image", image)
+        identify_card(image)
         cv2.waitKey(0)
+
+
+def identify_card(image):
+    """
+    Hash the identified card from the camera and compare it with the hashes previously generated
+
+    TODO: Completely update logic, process more images at once, use database
+    """
+    pil_image = Image.fromarray(image)
+    pil_image_hash = imagehash.phash(pil_image)
+    min_dist = -1
+    matched_card = ''
+    with open('card_sets/sv4pt5/sv4pt5.json') as f:
+        card_data = json.load(f)
+
+    for card in card_data:
+        card_hash = imagehash.hex_to_hash(card['hash'])  # Convert the hash string back to ImageHash
+        dist = pil_image_hash - card_hash  # Calculate Hamming distance
+        if min_dist == -1 or dist < min_dist:
+            min_dist = dist
+            matched_card = card['card_id']
+
+    print(min_dist, matched_card)
+    response = requests.get("https://api.pokemontcg.io/v2/cards/" + matched_card, headers=headers)
+    print(response.json())
 
 
 # https://docs.opencv.org/4.9.0/d4/d73/tutorial_py_contours_begin.html
@@ -121,18 +148,19 @@ def find_contours(image):
     get_rectangular_contours function
 
     Args:
-        image (array): Camera image
+        image: Camera image
 
     Returns:
-        contours (array): Array of contours containing the card images
+        contours: Array of contours containing the card images
     """
     image_grayscale = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
 
     ret, thresh = cv2.threshold(image_grayscale, 127, 255, 0)
     contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     contours = get_rectangular_contours(contours)
-    cv2.drawContours(image, contours, -1, (0, 255, 0), 3)
-    cv2.imshow("Image", image)
+    image_copy = image.copy()
+    cv2.drawContours(image_copy, contours, -1, (0, 255, 0), 3)
+    cv2.imshow("Image", image_copy)
     cv2.waitKey(0)
     return contours
 
@@ -143,32 +171,79 @@ def get_rectangular_contours(contours):
     Tries to find all rectangular contours, supposedly containing Pokémon Cards.
 
     Args:
-        contours (array): Contours from cv2.findContours function
+        contours: Contours from cv2.findContours function
 
     Returns:
-        rectangular_contours (array): Array of contours containing the card images
+        rectangular_contours: Array of contours containing the card images
     """
     rectangular_contours = []
     for i in contours:
         area = cv2.contourArea(i)
         if area > 4000:  # Test  value for minimum threshold
             peri = cv2.arcLength(i, True)  # Get perimeter of contour
-            approx = cv2.approxPolyDP(i, 0.02 * peri, True)  # Gets number of sides of contour
+            approx = cv2.approxPolyDP(i, 0.05 * peri, True)  # Gets number of sides of contour
             if len(approx) == 4:
                 rectangular_contours.append(approx)
     return rectangular_contours
 
 
 def extract_card_images_from_image(contours, image):
+    """
+    Args:
+        contours: Rectangular contours containing playing cards from get_rectangular_contours function
+        image: Original image
+
+    Returns:
+        images: Array of card images
+    """
     images = []
     for contour in contours:
-        x, y, w, h = cv2.boundingRect(contour)
-        extracted_image = image[y:y + h, x:x + w]
-        perspective_transform_card()  # TBD
+        extracted_image = perspective_transform_card(contour, image)
         images.append(extracted_image)
     return images
 
 
 # https://docs.opencv.org/3.4/da/d6e/tutorial_py_geometric_transformations.html
-def perspective_transform_card():
-    pass
+# https://stackoverflow.com/questions/7263621/how-to-find-corners-on-a-image-using-opencv
+# https://docs.opencv.org/3.1.0/dd/d49/tutorial_py_contour_features.html !
+def perspective_transform_card(contour, image):
+    """
+    Transforms the perspective of the rectangular contour containing a playing card returning a "full screen" scan of
+    the card
+
+    Args:
+        contour: Rectangular contours containing a playing card
+        image: Original image
+
+    Returns:
+        res: Perspective transformed image
+    """
+    card_width = 733
+    card_height = 1024
+    contour = fix_contour(contour)
+    pts1 = np.float32([contour[0], contour[1], contour[3], contour[2]])
+    pts2 = np.float32([[0, 0], [card_width, 0], [0, card_height], [card_width, card_height]])
+    m = cv2.getPerspectiveTransform(pts1, pts2)
+    res = cv2.warpPerspective(image, m, (733, 1024))
+
+    return res
+
+
+def fix_contour(contour):
+    """
+    Rotates the contour array putting in position 0 the top left corner of the card
+
+    Args:
+        contour: Rectangular contours containing a playing card
+
+    Returns:
+        contour: Contour array with fixed corner order
+    """
+    dist1 = np.linalg.norm(contour[0] - contour[1])
+    dist2 = np.linalg.norm(contour[1] - contour[2])
+
+    if dist1 < dist2:  # Card is tilted to the right, position 0 is top-left:
+        pass
+    else:  # Card is tilted to the left, position 0 is top-right, needs to be rotated
+        contour = np.roll(contour, 2)
+    return contour
